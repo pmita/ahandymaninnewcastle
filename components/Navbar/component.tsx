f65ce887ba8a1c4e922393b298795c3ff841573c
panosVegas <panos.mita@leovegas.com>
2023-02-09 22:43:26 +0000

    adding svg components

--- components/Navbar/component.tsx
+++ components/Navbar/component.tsx
@@ -1,10 +1,26 @@
+import Link from 'next/link';
+//COMPONENTS
+import BurgerMenuSVG from '../SVGIcons/BurgerMenuSVG';
+import DarkModeSVG from '../SVGIcons/DarkModeSVG';
 //STYLES
 import styles from './styles.module.scss';
 
 const Navbar = () => {
     return(
         <nav className={styles.navbar}>
-            <h3 className={styles.navbarLogo}>A handyman in Newcastle</h3>
+            <h3 className={styles.navbarLogo}>
+                A handyman in Newcastle
+            </h3>
+            <ul className={styles.navbarActions}>
+                <DarkModeSVG height="25px" width="25px" fill="red" />
+                <Link href="/work">Our Work</Link>
+                <Link href="/contact">
+                    <button className="btn primary">
+                        Contact Us
+                    </button>
+                </Link>
+            </ul>
+            <BurgerMenuSVG height='20px' width='20px' fill="red" />
         </nav>
     );
 }
