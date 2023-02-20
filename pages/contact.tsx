2b994ebba8fe13957ec4b40e2106dacfb9384e16
panosVegas <panos.mita@leovegas.com>
2023-02-20 18:06:51 +0000

    expanding zod validation and furthering the contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,6 +9,8 @@ import Button from '../components/Button';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
+//ASSETS
+import { Locations } from '../variables/Locations';
 // STYLES
 import styles from '../styles/pages/contactPage.module.scss';
 
@@ -22,6 +24,27 @@ export const validationSchema = z.object({
         .string()
         .min(2, { message: "Last name must be at least 2 characters" })
         .max(20, { message: "Last name must be less than 20 characters" }),
+    email: z
+        .string()
+        .min(1, { message: "Email is required" })
+        .min(5, { message: "Email must be at least 5 characters" })
+        .email({ message: "Please enter a valid email" }),
+    mobilePhone: z
+        .string()
+        .min(1, { message: "Mobile phone is required" })
+        .min(10, { message: "Mobile phone must be at least 10 characters" })
+        .max(10, { message: "Mobile phone must be less than 11 characters" })
+        .regex(/^[0-9]+$/, { message: "Mobile phone must be a number" }),
+    location: z
+        .string()
+        .min(1, { message: "Location is required" })
+        .refine((value) => {
+            return Locations.some((location) => location.value === value);
+        }, { message: "Please select a valid location" }),
+    additionalDetails: z
+        .string()
+        .min(10, { message: "Additional info must be at least 10 characters"})
+        .max(500, { message: "Additional info must be less than 500 characters" })
 });
 
 export type FormValues = z.infer<typeof validationSchema>;
@@ -44,19 +67,66 @@ const ContactPage = () => {
             <SplitBanner>
                 <BannerImage imageUrl="/assets/images/PaintBrush.jpg"/>
                 <BannerContent>
-                    <form>
-                        <input
-                            type="text"
-                            placeholder="First Name"
-                            {...register("firstName")}
-                        />
+                    <form className={styles.form}>
+                        <div className={styles.contactDetails}>
+                            <input
+                                type="text"
+                                placeholder="First Name"
+                                {...register("firstName")}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Last Name"
+                                {...register("lastName")}
+                                />  
+                        </div>
+                        
                         {errors.firstName && <p>{errors.firstName.message}</p>}
-                        <input
-                            type="text"
-                            placeholder="Last Name"
-                            {...register("lastName")}
-                        />  
                         {errors.lastName && <p>{errors.lastName.message}</p>}
+                        
+                        <div className={styles.contactDetails}>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                {...register("email")}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Mobile Phone"
+                                {...register("mobilePhone")}
+                                />  
+                        </div>
+                        
+                        {errors.email && <p>{errors.email.message}</p>}
+                        {errors.mobilePhone && <p>{errors.mobilePhone.message}</p>}
+                        
+                        <div className={styles.locationDetails}>
+                            <select {...register("location")}>
+                                {Locations.map((location) => (
+                                    <option key={location.id} value={location.value}>{location.option}</option>
+                                ))}
+                            </select>
+                        </div>
+                        
+                        {errors.location && <p>{errors.location.message}</p>}
+                        
+                        <div className={styles.additionalDetails}>
+                            <textarea
+                                placeholder="Tell us mores"
+                                {...register("additionalDetails")}
+                            />
+                        </div>
+                        
+                        {errors.location && <p>{errors.location.message}</p>}
+
+                        <div className={styles.callToAction}>
+                            <button className="btn secondary" type="submit">
+                                Submit
+                            </button>
+                            <h4>
+                                or <span>email@google.com</span>
+                            </h4>
+                        </div>
                     </form>
                 </BannerContent>
             </SplitBanner>
---
+++ variables/Locations.tsx
@@ -0,0 +1,7 @@
+export const Locations = [
+    {id: "1", value: "Gateshead", option: "Gateshead"},
+    {id: "2", value: "Newcastle", option: "Newcastle"},
+    {id: "3", value: "Sunderland", option: "Sunderland"},
+    {id: "4", value: "Durham", option: "Durham"},
+    {id: "5", value: "Heaton", option: "Heaton"},
+]
