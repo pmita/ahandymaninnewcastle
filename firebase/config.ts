92c1f2f7a4b2fc2b9f31e94ce274b55687436814
pmita <panos.mita2@outlook.com>
2023-02-21 14:38:11 +0000

    setting up firebase

---
+++ firebase/config.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.FIREBASE_APP_ID}`
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const firestore = firebase.firestore();
